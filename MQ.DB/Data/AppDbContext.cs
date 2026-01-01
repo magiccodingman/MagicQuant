af122ce9cb2efb870e848721813414bf98563ed8
MagicCodingMan <magiccodingman@gmail.com>
2026-01-01 15:23:29 -0500

    Basic SQLite code first pattern.

--- MQ.DB/Data/AppDbContext.cs
+++ MQ.DB/Data/AppDbContext.cs
@@ -6,21 +6,11 @@ namespace MQ.DB.Data;
 
 public class AppDbContext : DbContext
 {
-    // This represents a table in your DB. Add more DbSets here as you create models.
-    // public DbSet<Trade> Trades { get; set; } 
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        // 1. Get the directory from your existing static Cache
         var directory = Cache.MagicQuantDirectory;
-            
-        // 2. Combine with the filename
         var dbPath = Path.Combine(directory, "MagicQuant_SQLite.db");
-
-        // 3. Configure SQLite
-        // EF Core for SQLite enables Foreign Keys by default (PRAGMA foreign_keys = ON),
-        // so you don't typically need extra configuration for that, but it handles it here.
-        optionsBuilder.UseSqlite($"Data Source={dbPath}");
+        optionsBuilder.UseSqlite($"Data Source={dbPath};Foreign Keys=True;");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
