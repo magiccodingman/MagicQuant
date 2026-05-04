dcf638560522447ba9a077a92997daaa8c3dac97
MagicCodingMan <magiccodingman@gmail.com>
2026-05-04 15:52:06 -0400

    back to 27B testing. Q8 is broke in predictive space now, working on it now.

--- MagicQuant/Program.cs
+++ MagicQuant/Program.cs
@@ -33,7 +33,7 @@ if (args.Length == 0)
         args =
         [
             "evolution",
-            "--architecture-family", @"""Qwen3-4B"""
+            "--architecture-family", @"""Qwen3.6-27B"""
             ,"--reuse-existing-final-artifacts"
         ];
     }
